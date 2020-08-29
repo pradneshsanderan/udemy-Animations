2c8f6d35c298d830fe397badf04c0ac45a4e4e9d
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-08-29 18:18:51 +0100

    transition events

--- anim/src/App.js
+++ anim/src/App.js
@@ -27,7 +27,13 @@ class App extends Component {
         <h1>React Animations</h1>
         <button className="Button" onClick={() => this.setState(prevState => ({ showBlock: !prevState.showBlock }))}>Toggle</button>
         <br />
-        <Transition in={this.state.showBlock} timeout={300} mountOnEnter unmountOnExit>
+        <Transition in={this.state.showBlock} timeout={300} mountOnEnter unmountOnExit 
+        onEnter={() => console.log('onEnter')}
+        onEntering={() => console.log('onEntering')}
+        onEntered={() => console.log('onEntered')}
+        onExit={() => console.log('onExit')}
+        onExiting={() => console.log('onExiting')}
+        onExited={() => console.log('onExited')}>
           {state => (
              <div style={{
               backgroundColor: "red",
