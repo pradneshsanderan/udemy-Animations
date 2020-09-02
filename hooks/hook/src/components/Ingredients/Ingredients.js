206d8a545cf98cf52eb57c25a70b1813d456efa1
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-02 23:18:55 +0800

    sending http request

--- hooks/hook/src/components/Ingredients/Ingredients.js
+++ hooks/hook/src/components/Ingredients/Ingredients.js
@@ -4,22 +4,32 @@ import IngredientForm from './IngredientForm';
 import Search from './Search';
 
 function Ingredients() {
-  const [ userIngredients, setUserIngredients] = useState([]);
+  const [userIngredients, setUserIngredients] = useState([]);
   const removeIngredientHandler = ingredientId => {
-    setUserIngredients(prevIngredients=>prevIngredients.filter((
+    setUserIngredients(prevIngredients => prevIngredients.filter((
       ingredient) => ingredient.id !== ingredientId));
   };
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients,
-       {id:Math.random().toString(), ...ingredient }]);
-  }
+    fetch('https://hooks-2814c.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [...prevIngredients,
+      { id: responseData.name, ...ingredient }]
+      );
+    });
+
+  };
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={()=> {removeIngredientHandler}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {removeIngredientHandler}} />
       </section>
     </div>
   );
