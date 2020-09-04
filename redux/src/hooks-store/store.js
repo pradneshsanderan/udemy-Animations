2f7ce4405270450e4b654b64bc2bf18e3733b7d8
Pradnesh Sanderan <pradneshsanderan@gmail.com>
2020-09-04 23:52:10 +0800

    store hook

--- redux/src/hooks-store/store.js
+++ redux/src/hooks-store/store.js
@@ -5,8 +5,15 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-const useStore = () => {
+export const useStore = () => {
     const setState = useState(globalState)[1];
+    const dispatch = actionIdentifier => {
+        const newState = actions[actionIdentifier](globalState);
+        globalState = {...globalState, ...newState};
+        for (const listener of listeners){
+            listener(globalState);
+        }
+    };
     useEffect(() => {
         listeners.push(setState);
         return () => {
@@ -14,5 +21,14 @@ const useStore = () => {
         }
     }, [setState]);
 
+    return [globalState, dispatch];
 
+
+};
+
+export const initStore = (userActions, initialState) => {
+    if(initialState){
+        globalState = {...globalState, ...initialState};
+    }
+    actions = {...actions, ...userActions};
 };
